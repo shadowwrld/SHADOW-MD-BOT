cff10e4237fa0f7e8ea349b3ad8f98d0017dd84d
🌹⃝⃞⃟𝚂𝙷𝙰𝙳𝙾𝚆𝄟✮͢≛⃝𝚆𝚁𝙻𝙳⃝⃞⃟🌹 <164931149+ShadowWrld@users.noreply.github.com>
2024-11-23 23:15:31 -0500

    𝟐𝐫𝐝 𝐖𝐀𝐕𝐄 𝐎𝐅 𝐒𝐇𝐀𝐃𝐎𝐖 ʕʘ̅͜ʘ̅ʔ

--- commandes/Updates.js
+++ commandes/Updates.js
@@ -2,7 +2,6 @@ const { zokou } = require("../framework/zokou");
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
-const { exec } = require("child_process");
 
 zokou({
   nomCom: "update",
@@ -11,7 +10,7 @@ zokou({
   desc: "Check for new commits and update the bot",
   alias: ["upgrade"]
 }, async (origineMessage, zk, commandeOptions) => {
-  const { ms, msgRepondu, arg, repondre, nomAuteurMessage } = commandeOptions;
+  const { repondre } = commandeOptions;
 
   zk.sendMessage(origineMessage, { text: "ᴄᴏᴍᴍᴀɴᴅᴇ ᴅᴇᴠᴇʟᴏᴘᴘÉ ᴘᴀʀ sʜᴀᴅᴏᴡ-ᴡʀʟᴅ" });
 
@@ -19,21 +18,21 @@ zokou({
     const repoUrl = "https://api.github.com/repos/ShadowWrld/SHADOW-MD-BOT";
     const commitsUrl = `${repoUrl}/commits`;
 
-    // Cette partie cherche les commits
+    // Fetching the latest commits
     const commitsResponse = await axios.get(commitsUrl);
     const latestCommit = commitsResponse.data[0];
 
-    // Cette partie cherche les informations du dernier commit sur ton repo
+    // Fetching the details of the latest commit
     const commitDetailsUrl = latestCommit.url;
     const commitDetailsResponse = await axios.get(commitDetailsUrl);
     const filesChanged = commitDetailsResponse.data.files;
 
-    // Là ça cherche s’il y a des fichiers modifiés
+    // Checking for modified files
     const localFiles = getLocalFiles(".");
     const filesToUpdate = filesChanged.filter(file => localFiles.includes(file.filename));
 
     if (filesToUpdate.length === 0) {
-      return repondre("ᴘᴀs ᴅᴇ ᴍɪsᴇ à ᴊᴏᴜʀ ᴅéᴛᴇᴄᴛé ᴘᴏᴜʀ ʟᴇ ᴍᴏᴍᴇɴᴛ.ᴘᴀs ᴅᴇ ᴍɪsᴇ à ᴊᴏᴜʀ ᴅéᴛᴇᴄᴛé ᴘᴏᴜʀ ʟᴇ ᴍᴏᴍᴇɴᴛ.");
+      return repondre("ᴘᴀs ᴅᴇ ᴍɪsᴇ à ᴊᴏᴜʀ ᴅéᴛᴇᴄᴛé ᴘᴏᴜʀ ʟᴇ ᴍᴏᴍᴇɴᴛ.");
     }
 
     repondre(`Mise à jour nécessaire pour les(s) fichiers : ${filesToUpdate.map(file => file.filename).join(", ")}`);
